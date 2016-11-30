e5b3cec972ef0d9eae5dc954817fb7607d48e463
Adam Rafajdus <adam.rafajdus@gmail.com>
2016-11-30 14:02:56 +0100

    Change stuff - still doesnt work
    Signed-off-by: Adam Rafajdus <adam.rafajdus@gmail.com>

--- cherry.py
+++ cherry.py
@@ -12,43 +12,85 @@ class DateHelper(object):
         return open('index.html')
 
 
-@cherrypy.expose
-class DateHelperWebService(object):
-
-
-    @cherrypy.tools.accept(media='text/plain')
-    def GET(self):
-        return cherrypy.session['mystring']
-
-    def POST(self, length=8):
-        some_string = ''.join(random.sample(string.hexdigits, int(length)))
-        cherrypy.session['mystring'] = some_string
-        return some_string
-
-    def PUT(self, another_string):
-        cherrypy.session['mystring'] = another_string
-
-    def DELETE(self):
-        cherrypy.session.pop('mystring', None)
-
-class Newsagents:
+class Api(object):
 
     exposed = True
+    conn = psycopg2.connect("dbname=gisproject user=floofy")
 
-    def GET(self):
-        conn = psycopg2.connect("dbname=gisproject user=floofy")
+    def POST(self, item, lng, lat, distance):
         cur = conn.cursor()
-        cur.execute("""
-            with MHDtickets as (
-            select name, amenity, shop, way from planet_osm_polygon
-            where shop = 'newsagent'
-            union
-            select name, amenity, shop, way from planet_osm_point
-            where shop = 'newsagent')
-            select ST_AsGeoJSON(st_transform(ST_SetSRID(m.way, 4326), 4326))::json from MHDtickets m
-            """)
+
+        if distance == "":
+            distance = 500
+        else: distance = str(distance)
+
+
+        if item == "newsagent":
+            cur.execute("""
+                with MHDtickets as (
+                select name, amenity, shop, way from planet_osm_polygon
+                where shop = 'newsagent'
+                union
+                select name, amenity, shop, way from planet_osm_point
+                where shop = 'newsagent')
+                select ST_AsGeoJSON(ST_Transform(m.way, 4326))::json as way from MHDtickets m
+                """)
+        elif item == "supermarket":
+            cur.execute("""
+                with supermarket as (
+                select name, amenity, shop, way from planet_osm_polygon
+                where shop = 'supermarket'
+                or shop = 'convenience'
+                union
+                select name, amenity, shop, way from planet_osm_point
+                where shop = 'supermarket'
+                or shop = 'convenience')
+                select ST_AsGeoJSON(ST_Transform(s.way, 4326))::json as way from supermarket s
+                """)
+        elif item == "flowers":
+            cur.execute("""
+                with flowers as (
+                select name, amenity, shop, way from planet_osm_polygon
+                where shop = 'florist'
+                union
+                select name, amenity, shop, way from planet_osm_point
+                where shop = 'florist')
+                select ST_AsGeoJSON(ST_Transform(f.way, 4326))::json as way from flowers f
+                """)
+        elif item == "gas_station":
+            cur.execute("""
+                with gas as (
+                select name, amenity, shop, way from planet_osm_polygon
+                where shop = 'fuel'
+                union
+                select name, amenity, shop, way from planet_osm_point
+                where shop = 'fuel')
+                select ST_AsGeoJSON(ST_Transform(g.way, 4326))::json as way from gas g
+                """)
+        elif item == "park_paths":
+            cur.execute("""
+                with walkpaths as (
+                        select name, way, highway from public.planet_osm_line
+                        where highway = 'path'
+                        or highway = 'footway'
+                        ),
+                forests as (
+                        select name, way, leisure from public.planet_osm_polygon
+                        WHERE leisure = 'park'
+                        )
+                select ST_AsGeoJSON(ST_Transform(f.way, 4326))::json from walkpaths p, forests f
+                where st_intersects(p.way, f.way)
+                """)
+
+
         data = cur.fetchall()
         reply = json.dumps(data)
+        return reply 
+
+    def GET(self):
+        with open('/tmp/t.json') as data_f:
+            data  = json.load(data_f)
+            reply = json.dumps(data)
         return reply 
 
 
@@ -61,7 +103,6 @@ if __name__ == '__main__':
         '/generator': {
             'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
             'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
         },
         '/static': {
             'tools.staticdir.on': True,
@@ -70,13 +111,12 @@ if __name__ == '__main__':
     }
 
     cherrypy.tree.mount(
-        Newsagents(), '/api/newsagents',
+        Api(), '/api/',
         {'/':
             {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
         }
     )
 
     webapp = DateHelper()
-    webapp.generator = DateHelperWebService()
 
     cherrypy.quickstart(webapp, '/', conf)
--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,102 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiZmxvb2Z5IiwiYSI6ImNpdTZxazU4czAwMmMyb3M0dHpwdGFhNjYifQ.-uNsNCTSDE3y9VSjAsKj3A';
-var map = L.mapbox.map('map', 'mapbox.streets')
-    .setView([48.154, 17.072], 15);
 
+var map = L.mapbox.map('map','mapbox.streets').setView([48.154, 17.072], 15);
 
 var featureLayer = L.mapbox.featureLayer().addTo(map);
+
+
+$('#btn_shop').on('click', function () {
+    var vars = {};
+    if ($('ch1').is(':checked')) {
+    vars["item"] = "supermarket";
+    }
+    if ($('ch2').is(':checked')) {
+    vars["item"] = "newsagent";
+    }
+    if ($('ch3').is(':checked')) {
+    vars["item"] = "flowers";
+    }
+    vars["lng"] = "",
+    vars["lat"] = "",
+    vars["distance"] = "",
+    $.ajax({
+        type: 'POST',
+        url: "http://localhost:8080/api/",
+        data: vars,
+        datatype: "json",
+        success: function (result) {
+            console.log('start');
+            console.log(url);
+
+            //featureLayer.setGeoJSON(result[0]);
+            // map.addSource('point', {
+            //     "type": "geojson",
+            //     "data": result[0]
+            // });
+            // map.addLayer({
+            //   "id": name,
+            //   "type": "line",
+            //   "source": 'point',
+            //   "layout": {
+            //       "line-join": "round",
+            //       "line-cap": "round"
+            //   },
+            //   "paint": {
+            //       "line-color": colour,
+            //       "line-width": 8
+            //   }
+            //   });
+            console.log('stop');
+        }
+    });
+});
+
+$('#btn_parks').on('click', function () {
+    var vars = {};
+    vars["item"] = "park_paths";
+    vars["lng"] = "",
+    vars["lat"] = "",
+    vars["distance"] = "",
+    $.ajax({
+        type: 'POST',
+        url: "http://localhost:8080/api/",
+        data: vars,
+        datatype: "json",
+        success: function (result) {
+            console.log('start');
+            console.log(url);
+
+            //featureLayer.setGeoJSON(result[0]);
+            // map.addSource('point', {
+            //     "type": "geojson",
+            //     "data": result[0]
+            // });
+            // map.addLayer({
+            //   "id": name,
+            //   "type": "line",
+            //   "source": 'point',
+            //   "layout": {
+            //       "line-join": "round",
+            //       "line-cap": "round"
+            //   },
+            //   "paint": {
+            //       "line-color": colour,
+            //       "line-width": 8
+            //   }
+            //   });
+            console.log('stop');
+        }
+    });
+});
+        //data: params,
+
+function parse_to_geoJSON(geometry, description) {
+    geojson = {
+            "type": "Feature",
+            "geometry": geometry,
+            "properties": {
+                "description": description,
+            }
+    };
+    return geojson;
+}
