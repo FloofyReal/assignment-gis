4af1b509802d8dff50136a2d95bc1f1b52364efc
Adam Rafajdus <adam.rafajdus@gmail.com>
2016-12-01 21:43:17 +0100

    Fix all - function as have to
    Signed-off-by: Adam Rafajdus <adam.rafajdus@gmail.com>

--- public/js/map.js
+++ public/js/map.js
@@ -1,102 +1,70 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiZmxvb2Z5IiwiYSI6ImNpdTZxazU4czAwMmMyb3M0dHpwdGFhNjYifQ.-uNsNCTSDE3y9VSjAsKj3A';
+var map = L.mapbox.map('map', 'mapbox.streets')
+    .setView([48.154, 17.072], 15);
 
-var map = L.mapbox.map('map','mapbox.streets').setView([48.154, 17.072], 15);
 
 var featureLayer = L.mapbox.featureLayer().addTo(map);
 
 
 $('#btn_shop').on('click', function () {
-    var vars = {};
-    if ($('ch1').is(':checked')) {
-    vars["item"] = "supermarket";
-    }
-    if ($('ch2').is(':checked')) {
-    vars["item"] = "newsagent";
-    }
-    if ($('ch3').is(':checked')) {
-    vars["item"] = "flowers";
-    }
+    vars = {}
     vars["lng"] = "",
     vars["lat"] = "",
     vars["distance"] = "",
     $.ajax({
-        type: 'POST',
-        url: "http://localhost:8080/api/",
-        data: vars,
+        type: 'GET',
+        url: "http://localhost:8080/api/newsagents",
         datatype: "json",
         success: function (result) {
             console.log('start');
-            console.log(url);
-
-            //featureLayer.setGeoJSON(result[0]);
-            // map.addSource('point', {
-            //     "type": "geojson",
-            //     "data": result[0]
-            // });
-            // map.addLayer({
-            //   "id": name,
-            //   "type": "line",
-            //   "source": 'point',
-            //   "layout": {
-            //       "line-join": "round",
-            //       "line-cap": "round"
-            //   },
-            //   "paint": {
-            //       "line-color": colour,
-            //       "line-width": 8
-            //   }
-            //   });
-            console.log('stop');
+            featureLayer.setGeoJSON(parse_to_geoJSON(result));
         }
     });
 });
 
 $('#btn_parks').on('click', function () {
-    var vars = {};
-    vars["item"] = "park_paths";
+    vars = {}
     vars["lng"] = "",
     vars["lat"] = "",
     vars["distance"] = "",
     $.ajax({
-        type: 'POST',
-        url: "http://localhost:8080/api/",
-        data: vars,
+        type: 'GET',
+        url: "http://localhost:8080/api/newsagents",
         datatype: "json",
         success: function (result) {
             console.log('start');
-            console.log(url);
-
-            //featureLayer.setGeoJSON(result[0]);
-            // map.addSource('point', {
-            //     "type": "geojson",
-            //     "data": result[0]
-            // });
-            // map.addLayer({
-            //   "id": name,
-            //   "type": "line",
-            //   "source": 'point',
-            //   "layout": {
-            //       "line-join": "round",
-            //       "line-cap": "round"
-            //   },
-            //   "paint": {
-            //       "line-color": colour,
-            //       "line-width": 8
-            //   }
-            //   });
-            console.log('stop');
+            featureLayer.setGeoJSON(parse_to_geoJSON(result));
         }
     });
 });
-        //data: params,
+
+$('#btn_shop').on('click', function () {
+    vars = {}
+    vars["lng"] = "",
+    vars["lat"] = "",
+    vars["distance"] = "",
+    $.ajax({
+        type: 'GET',
+        url: "http://localhost:8080/api/newsagents",
+        datatype: "json",
+        success: function (result) {
+            console.log('start');
+            featureLayer.setGeoJSON(parse_to_geoJSON(result));
+        }
+    });
+});
 
 function parse_to_geoJSON(geometry, description) {
     geojson = {
-            "type": "Feature",
-            "geometry": geometry,
-            "properties": {
-                "description": description,
-            }
-    };
+            "type": "FeatureCollection",
+            "features": JSON.parse(geometry).map(function(g){                        
+             return {
+                      type: "Feature",
+                      properties: {},
+                      geometry: g
+                    }
+             })
+    }
+    console.log(geojson);
     return geojson;
 }
