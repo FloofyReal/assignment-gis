eda5399e0f76c0d886504e0e422f68d913e2be69
Adam Rafajdus <adam.rafajdus@gmail.com>
2016-12-02 22:54:35 +0100

    Add first function (with sparkling shine)
    Signed-off-by: Adam Rafajdus <adam.rafajdus@gmail.com>

--- cherry.py
+++ cherry.py
@@ -13,27 +13,7 @@ class DateHelper(object):
 
 
 @cherrypy.expose
-class DateHelperWebService(object):
-
-
-    @cherrypy.tools.accept(media='text/plain')
-    def GET(self):
-        return cherrypy.session['mystring']
-
-    def POST(self, length=8):
-        some_string = ''.join(random.sample(string.hexdigits, int(length)))
-        cherrypy.session['mystring'] = some_string
-        return some_string
-
-    def PUT(self, another_string):
-        cherrypy.session['mystring'] = another_string
-
-    def DELETE(self):
-        cherrypy.session.pop('mystring', None)
-
-class Newsagents:
-
-    exposed = True
+class Newsagents(object):
 
     def GET(self):
         conn = psycopg2.connect("dbname=gisproject user=floofy")
@@ -45,25 +25,167 @@ class Newsagents:
             union
             select name, amenity, shop, way from planet_osm_point
             where shop = 'newsagent')
-            select count(ST_AsGeoJSON(st_transform(m.way, 4326))::json) from MHDtickets m
-            """)
-        num = cur.fetchone()
-        cur.execute("""
-            with MHDtickets as (
-            select name, amenity, shop, way from planet_osm_polygon
-            where shop = 'newsagent'
-            union
-            select name, amenity, shop, way from planet_osm_point
-            where shop = 'newsagent')
             select ST_AsGeoJSON(st_transform(m.way, 4326))::json from MHDtickets m
             """)
         listt = []
-        for i in range(num[0]):
-           data = cur.fetchone()
-           listt.append(data[0])
+        while True:
+            row = cur.fetchone()
+            if row == None:
+                break
+            else:
+                listt.append(row[0])
+                print("%s" % row[0]) 
         reply = json.dumps(listt)
-        print(reply)
         return reply 
+
+
+@cherrypy.expose
+class Supermarkets(object):
+
+    def GET(self):
+        conn = psycopg2.connect("dbname=gisproject user=floofy")
+        cur = conn.cursor()
+        cur.execute("""
+            with supermarkets as (
+                select name, amenity, shop, way from planet_osm_polygon
+                where shop = 'supermarket'
+                union
+                select name, amenity, shop, way from planet_osm_point
+                where shop = 'supermarket')
+            select ST_AsGeoJSON(st_transform(s.way, 4326))::json from supermarkets s
+            """)
+        listt = []
+        while True:
+            row = cur.fetchone()
+            if row == None:
+                break
+            else:
+                listt.append(row[0])
+                print("%s" % row[0]) 
+        reply = json.dumps(listt)
+        return reply 
+
+
+@cherrypy.expose
+class Flowers(object):
+
+    def GET(self):
+        conn = psycopg2.connect("dbname=gisproject user=floofy")
+        cur = conn.cursor()
+        cur.execute("""
+            with flowers as (
+                select name, amenity, shop, way from planet_osm_polygon
+                where shop = 'florist'
+                union
+                select name, amenity, shop, way from planet_osm_point
+                where shop = 'florist')
+            select ST_AsGeoJSON(st_transform(f.way, 4326))::json from flowers f
+            """)
+        listt = []
+        while True:
+            row = cur.fetchone()
+            if row == None:
+                break
+            else:
+                listt.append(row[0])
+                print("%s" % row[0]) 
+        reply = json.dumps(listt)
+        return reply 
+
+
+@cherrypy.expose
+class Gas(object):
+
+    def GET(self):
+        conn = psycopg2.connect("dbname=gisproject user=floofy")
+        cur = conn.cursor()
+        cur.execute("""
+            with gas as (
+                select name, amenity, shop, way from planet_osm_polygon
+                where shop = 'fuel'
+                union
+                select name, amenity, shop, way from planet_osm_point
+                where amenity = 'fuel')
+            select ST_AsGeoJSON(st_transform(g.way, 4326))::json from gas g
+            """)
+        listt = []
+        while True:
+            row = cur.fetchone()
+            if row == None:
+                break
+            else:
+                listt.append(row[0])
+                print("%s" % row[0]) 
+        reply = json.dumps(listt)
+        return reply 
+
+
+
+@cherrypy.expose
+class Parks(object):
+
+    def GET(self):
+        conn = psycopg2.connect("dbname=gisproject user=floofy")
+        cur = conn.cursor()
+        cur.execute("""
+        with walkpaths as (
+            select name, way, highway from public.planet_osm_line
+	    where highway = 'path'
+	    or highway = 'footway'
+	),
+        forests as (
+	    select name, way, leisure from public.planet_osm_polygon
+	    where leisure = 'park'
+	)
+        select ST_AsGeoJSON(ST_Transform(f.way, 4326))::json from walkpaths p, forests f
+        where st_intersects(p.way, f.way)
+            """)
+        listt = []
+        while True:
+            row = cur.fetchone()
+            if row == None:
+                break
+            else:
+                listt.append(row[0])
+                print("%s" % row[0]) 
+        reply = json.dumps(listt)
+        return reply 
+
+
+cherrypy.tree.mount(
+    Newsagents(), '/api/newsagents',
+    {'/':
+        {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+    }
+)
+
+cherrypy.tree.mount(
+    Supermarkets(), '/api/supermarkets',
+    {'/':
+        {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+    }
+)
+
+cherrypy.tree.mount(
+    Flowers(), '/api/flowers',
+    {'/':
+        {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+    }
+)
+
+cherrypy.tree.mount(
+    Gas(), '/api/gas',
+    {'/':
+        {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+    }
+)
+
+cherrypy.tree.mount(
+    Parks(), '/api/parks',
+    {'/':
+        {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+    }
+)
 
 
 if __name__ == '__main__':
@@ -72,25 +194,12 @@ if __name__ == '__main__':
             'tools.sessions.on': True,
             'tools.staticdir.root': os.path.abspath(os.getcwd())
         },
-        '/generator': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
-        },
         '/static': {
             'tools.staticdir.on': True,
             'tools.staticdir.dir': './public'
         }
     }
 
-    cherrypy.tree.mount(
-        Newsagents(), '/api/newsagents',
-        {'/':
-            {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
-        }
-    )
-
     webapp = DateHelper()
-    webapp.generator = DateHelperWebService()
 
     cherrypy.quickstart(webapp, '/', conf)
--- public/js/map.js
+++ public/js/map.js
@@ -4,22 +4,68 @@ var map = L.mapbox.map('map', 'mapbox.streets')
 
 
 var featureLayer = L.mapbox.featureLayer().addTo(map);
+var shopLayer = L.mapbox.featureLayer().addTo(map);
+var floristLayer = L.mapbox.featureLayer().addTo(map);
+var gasLayer = L.mapbox.featureLayer().addTo(map);
+var newsagentsLayer = L.mapbox.featureLayer().addTo(map);
 
+    if('spm' === 'gas'){
+        console.log("Piča");
+    }
 
 $('#btn_shop').on('click', function () {
+    var supermarket = document.getElementById("chs01").checked;
+    var newsagents = document.getElementById("chs02").checked;
+    var flowers = document.getElementById("chs03").checked;
+    var gas = document.getElementById("chs04").checked;
     vars = {}
-    vars["lng"] = "",
-    vars["lat"] = "",
-    vars["distance"] = "",
+    vars["lng"] = "";
+    vars["lat"] = "";
+    vars["distance"] = "";
+    if(supermarket) {
+    $.ajax({
+        type: 'GET',
+        url: "http://localhost:8080/api/supermarkets",
+        datatype: "json",
+        success: function (result) {
+            console.log('start');
+            shopLayer.setGeoJSON(parse_to_geoJSON(result, 'spm'));
+        }
+    });
+    };
+    if(newsagents) {
     $.ajax({
         type: 'GET',
         url: "http://localhost:8080/api/newsagents",
         datatype: "json",
         success: function (result) {
             console.log('start');
-            featureLayer.setGeoJSON(parse_to_geoJSON(result));
+            newsagentsLayer.setGeoJSON(parse_to_geoJSON(result, 'nws'));
         }
     });
+    };
+    if(flowers) {
+    $.ajax({
+        type: 'GET',
+        url: "http://localhost:8080/api/flowers",
+        datatype: "json",
+        success: function (result) {
+            console.log('start');
+            floristLayer.setGeoJSON(parse_to_geoJSON(result, 'flw'));
+        }
+    });
+    };
+    if(gas) {
+    $.ajax({
+        type: 'GET',
+        url: "http://localhost:8080/api/gas",
+        datatype: "json",
+        success: function (result) {
+            console.log('start');
+            gasLayer.setGeoJSON(parse_to_geoJSON(result, 'gas'));
+        }
+    });
+    };
 });
 
 $('#btn_parks').on('click', function () {
@@ -29,7 +75,7 @@ $('#btn_parks').on('click', function () {
     vars["distance"] = "",
     $.ajax({
         type: 'GET',
-        url: "http://localhost:8080/api/newsagents",
+        url: "http://localhost:8080/api/parks",
         datatype: "json",
         success: function (result) {
             console.log('start');
@@ -38,33 +84,112 @@ $('#btn_parks').on('click', function () {
     });
 });
 
-$('#btn_shop').on('click', function () {
-    vars = {}
-    vars["lng"] = "",
-    vars["lat"] = "",
-    vars["distance"] = "",
-    $.ajax({
-        type: 'GET',
-        url: "http://localhost:8080/api/newsagents",
-        datatype: "json",
-        success: function (result) {
-            console.log('start');
-            featureLayer.setGeoJSON(parse_to_geoJSON(result));
-        }
-    });
-});
-
-function parse_to_geoJSON(geometry, description) {
+function parse_to_geoJSON(geometry, title) {
+    var spm = 'spm';
+    var nws = 'nws';
+    var flw = 'flw';
+    var gas = 'gas';
+    if(title === spm){
     geojson = {
             "type": "FeatureCollection",
             "features": JSON.parse(geometry).map(function(g){                        
              return {
-                      type: "Feature",
-                      properties: {},
-                      geometry: g
+                      "type": "Feature",
+                      "properties": {
+                        "title": "Supermarket",
+                        "marker-color": "#d71f1f",
+                        "marker-size": "medium",
+                        "marker-symbol": "shop",
+                        "fill": "#d71f1f",
+                        "fill-opacity": 0.5,
+                        "stroke": "#555555",
+                        "stroke-width": 2,
+                        "stroke-opacity": 0.2
+                      },
+                      "geometry": g
                     }
              })
     }
-    console.log(geojson);
+    } else if(title === nws){
+    geojson = {
+            "type": "FeatureCollection",
+            "features": JSON.parse(geometry).map(function(g){                        
+             return {
+                      "type": "Feature",
+                      "properties": {
+                        "title": "Trafika",
+                        "marker-color": "#3d68a5",
+                        "marker-size": "medium",
+                        "marker-symbol": "library",
+                        "fill": "#3d68a5",
+                        "fill-opacity": 0.5,
+                        "stroke": "#555555",
+                        "stroke-width": 2,
+                        "stroke-opacity": 0.2
+                      },
+                      "geometry": g
+                    }
+             })
+    }
+    } else if(title === flw){
+    geojson = {
+            "type": "FeatureCollection",
+            "features": JSON.parse(geometry).map(function(g){                        
+             return {
+                      "type": "Feature",
+                      "properties": {
+                        "title": "Kvetinárstvo",
+                        "marker-color": "#23983e",
+                        "marker-size": "medium",
+                        "marker-symbol": "garden",
+                        "fill": "#23983e",
+                        "fill-opacity": 0.5,
+                        "stroke": "#555555",
+                        "stroke-width": 2,
+                        "stroke-opacity": 0.2
+                      },
+                      "geometry": g
+                    }
+             })
+    }
+    } else if(title === gas){
+    geojson = {
+            "type": "FeatureCollection",
+            "features": JSON.parse(geometry).map(function(g){                        
+             return {
+                      "type": "Feature",
+                      "properties": {
+                        "title": "Pumpa",
+                        "marker-color": "#e9c33a",
+                        "marker-size": "medium",
+                        "marker-symbol": "fuel",
+                        "fill": "#e9c33a",
+                        "fill-opacity": 0.5,
+                        "stroke": "#555555",
+                        "stroke-width": 2,
+                        "stroke-opacity": 0.2
+                      },
+                      "geometry": g
+                    }
+             })
+    }
+    } else {
+    geojson = {
+            "type": "FeatureCollection",
+            "features": JSON.parse(geometry).map(function(g){                        
+             return {
+                      "type": "Feature",
+                      "properties": {
+                        "fill": "#704688",
+                        "fill-opacity": 0.5,
+                        "stroke": "#555555",
+                        "stroke-width": 2,
+                        "stroke-opacity": 0.2
+                      },
+                      "geometry": g
+                    }
+             })
+    }
+    }
     return geojson;
 }
